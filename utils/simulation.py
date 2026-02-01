9a772770a38ce3685740b80a5b1bbe19f0c2943f
sesteban951 <sesteban@caltech.edu>
2026-01-31 18:02:43 -0800

    added effective torque logging which includes saturations

--- utils/simulation.py
+++ utils/simulation.py
@@ -12,6 +12,7 @@ from typing import List
 import jax
 import jax.numpy as jnp
 from jax import lax
+from jax import tree_util
 
 # mujoco imports
 import mujoco
@@ -32,17 +33,13 @@ class ModelConfig:
     Kp: List[float]          # proportional gains for each joint
     Kd: List[float]          # derivative gain for each joint
 
-    # max and minimum torques
-    tau_limit: bool           # whether to use torque limits
-    tau_lb: List[float]       # minimum torques
-    tau_ub: List[float]       # maximum torques
-
     # actuated state indices
-    q_actuated_idx: List[int]   # indices of actuated positions
-    v_actuated_idx: List[int]   # indices of actuated velocities
+    q_actuated_idx: List[int]   # indices of actuated positions  (qpos)
+    v_actuated_idx: List[int]   # indices of actuated velocities (qvel)
 
     # action mode
-    action_mode: str = "tau"   # action mode: "tau" or "pos"
+    action_mode: str = "tau"   # action mode:    "tau" (pure torque) 
+                               #              or "pos" (PD tracking)
 
 
 # parallel sim config
@@ -104,8 +101,8 @@ class ParallelSim():
 
         # action mode
         self.use_pd = (model_config.action_mode == "pos")
-        self.q_actuated_idx = jnp.asarray(model_config.q_actuated_idx, dtype=jnp.int32)
-        self.v_actuated_idx = jnp.asarray(model_config.v_actuated_idx, dtype=jnp.int32)
+        self.q_actuated_idx = tuple(model_config.q_actuated_idx)
+        self.v_actuated_idx = tuple(model_config.v_actuated_idx)
         assert model_config.action_mode in ["tau", "pos"], "Invalid action mode."
         assert len(self.q_actuated_idx) == self.nu, "q_actuated_idx length does not match nu."
         assert len(self.v_actuated_idx) == self.nu, "v_actuated_idx length does not match nu."
@@ -113,17 +110,8 @@ class ParallelSim():
         # load control parameters
         assert len(model_config.Kp) == self.nu, "Kp length does not match nu."
         assert len(model_config.Kd) == self.nu, "Kd length does not match nu."
-        assert len(model_config.tau_lb) == self.nu, "tau_lb length does not match nu."
-        assert len(model_config.tau_ub) == self.nu, "tau_ub length does not match nu."
         self.Kp = jnp.array(model_config.Kp)
         self.Kd = jnp.array(model_config.Kd)
-        self.tau_limit = model_config.tau_limit
-        self.tau_lb = jnp.array(model_config.tau_lb)
-        self.tau_ub = jnp.array(model_config.tau_ub)
-
-        # create the batched step function
-        self.step_fn_batched = jax.jit(jax.vmap(lambda d: mjx.step(self.mjx_model, d), in_axes=0))
-        # self.step_fn_batched = jax.vmap(lambda d: mjx.step(self.mjx_model, d))
 
         # print message
         print(f"Initialized batched MJX model from [{model_config.xml_path}].")
@@ -136,12 +124,22 @@ class ParallelSim():
         """
         Initialize the functions with jit for rollout for speed.
         """
+
+        # create the batched step function
+        self.step_fn_batched = jax.jit(
+            jax.vmap(lambda d: mjx.step(self.mjx_model, d), in_axes=0)
+        )
+
+        # create a batched template of mjx_data 
+        self.data_batch0 = tree_util.tree_map(
+            lambda x: jnp.broadcast_to(x, (self.B,) + x.shape),
+            self.mjx_data
+        )
         
         # jit the rollout function
-        self.rollout = jax.jit(self.rollout)
-
-        # jit the pd torque function
-        # self._compute_pd_torque = jax.jit(self._compute_pd_torque)
+        self.rollout = jax.jit(
+            self.rollout
+        )
 
         print("JIT compilation of parallel sim function complete.")
 
@@ -160,7 +158,7 @@ class ParallelSim():
         Returns:
             q_log: jnp.array, logged positions,  shape (B, N+1, nq)
             v_log: jnp.array, logged velocities, shape (B, N+1, nv)
-            tau_log: jnp.array, logged torques, shape (N, B, nu)
+            tau_log: jnp.array, logged torques, shape (B, N, nu)
         """
 
         # get sizes
@@ -171,7 +169,7 @@ class ParallelSim():
         v0_batch = jnp.broadcast_to(v0, (B, self.nv))    # (B, nv)
 
         # set the initial conditions in the batched data
-        data0 = jax.vmap(lambda q, v: self.mjx_data.replace(qpos=q, qvel=v))(q0_batch, v0_batch)
+        data0 = self.data_batch0.replace(qpos=q0_batch, qvel=v0_batch)
 
         # swap axes for easier indexing (B, N, nu) -> (N, B, nu)
         U = jnp.swapaxes(U, 0, 1)
@@ -191,7 +189,10 @@ class ParallelSim():
             data = data.replace(ctrl=tau)        # set the control
             data = self.step_fn_batched(data)    # step
 
-            return data, (data.qpos, data.qvel, tau)
+            # effective applied torque (takes torque limits into account)
+            tau_effective = data.actuator_force
+
+            return data, (data.qpos, data.qvel, tau_effective)
 
         # forward propagate
         _, (q_hist, v_hist, tau_hist) = lax.scan(integration_step, data0, U, length=N)
@@ -203,8 +204,9 @@ class ParallelSim():
         # prepend initial state so logs are N+1
         q_log = jnp.concatenate([q0_batch[:, None, :], q_hist], axis=1)
         v_log = jnp.concatenate([v0_batch[:, None, :], v_hist], axis=1)
+        tau_log = jnp.swapaxes(tau_hist, 0, 1)
 
-        return q_log, v_log, tau_hist
+        return q_log, v_log, tau_log
     
     # compute PD torques
     def _compute_pd_torque(self, q, v, q_des):
@@ -218,19 +220,11 @@ class ParallelSim():
         """
 
         # extract actuated positions and velocities
-        q_act = jnp.take(q, self.q_actuated_idx, axis=1)  # (B, nu)
-        v_act = jnp.take(v, self.v_actuated_idx, axis=1)  # (B, nu)
+        q_act = q[:, self.q_actuated_idx]  # (B, nu)
+        v_act = v[:, self.v_actuated_idx]  # (B, nu)
 
         # compute PD torques
         tau = self.Kp[None, :] * (q_des - q_act) + self.Kd[None, :] * (-v_act) # (B, nu)
-
-        # clip the torques
-        tau = lax.cond(
-            self.tau_limit,  # Python bool (static)
-            lambda t: jnp.clip(t, self.tau_lb[None, :], self.tau_ub[None, :]),
-            lambda t: t,
-            tau
-        )
 
         return tau
 
@@ -245,14 +239,14 @@ import time
 
 if __name__ == "__main__":
 
+    # fix the random seed
+    np.random.seed(0)
+
     # model config
     model_config = ModelConfig(
         xml_path="./models/cartpole.xml",
         Kp=[300.0], 
-        Kd=[20.0],  
-        tau_limit=False,
-        tau_lb=[-500.0],
-        tau_ub=[ 500.0],
+        Kd=[40.0],  
         q_actuated_idx=[0],
         v_actuated_idx=[0],
         action_mode="pos"
@@ -268,7 +262,7 @@ if __name__ == "__main__":
     parallel_sim = ParallelSim(model_config, sim_config)
 
     # integration steps
-    N = 2000
+    N = 1000
 
     # initial conditions (cartpole: usually nq=2, nv=2)
     q0 = jnp.array([0.0, jnp.pi])  # slight offset from upright
@@ -294,12 +288,15 @@ if __name__ == "__main__":
     q_log, v_log, tau_log = parallel_sim.rollout(q0, v0, U)
     t3 = time.time()
     print(f"Rolled out {B} trajectories of length {N} in {t3 - t2:.4f} seconds.")
-    q_log, v_log, tau_log = parallel_sim.rollout(q0, v0, U)
-    t4 = time.time()
-    print(f"Rolled out {B} trajectories of length {N} in {t4 - t3:.4f} seconds.")
-    q_log, v_log, tau_log = parallel_sim.rollout(q0, v0, U)
-    t5 = time.time()
-    print(f"Rolled out {B} trajectories of length {N} in {t5 - t4:.4f} seconds.")
+
+    # convert to numpy for plotting
+    q_log = np.array(q_log)
+    v_log = np.array(v_log)
+    tau_log = np.array(tau_log)
+
+    print(f"q_log shape: {q_log.shape}")
+    print(f"v_log shape: {v_log.shape}")
+    print(f"tau_log shape: {tau_log.shape}")
 
     # choose a few trajectories to plot
     B = q_log.shape[0]
@@ -313,8 +310,9 @@ if __name__ == "__main__":
     plt.figure()
     for k in idx:
         plt.plot(t, q_log[k, :, 0], alpha=0.7)
-        # plt.plot(t, q_log[k, :, 1], alpha=0.7)
-        # plt.plot(t[:-1], tau_log[:, k, 0], alpha=0.7)
+        plt.plot(t, q_log[k, :, 1], alpha=0.7)
+        # plt.plot(t[:-1], tau_log[k, :, 0], alpha=0.7)
+
     plt.xlabel("Time (s)")
     plt.ylabel("Positions")
 
