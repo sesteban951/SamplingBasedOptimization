28264cb4f895852285a39e631b4be4de64857425
sesteban951 <sesteban@caltech.edu>
2026-02-02 17:02:00 -0800

    added base spline class

--- utils/spline.py
+++ utils/spline.py
@@ -4,18 +4,79 @@
 #  
 ##
 
+# for base class
+from __future__ import annotations
+from abc import ABC, abstractmethod
+
+# standard imports
+import numpy as np
+
 # jax imports
 import jax
 import jax.numpy as jnp
 
-import functools
-
 
 #############################################################
-# ZOH Spline
+# Base Spline Class
 #############################################################
 
-class ZOH_Spline:
+class Base_Spline(ABC):
+    """
+    Base class for control/trajectory splines.
+    """
+
+    # spline parameters
+    Y: jnp.ndarray  # spline points
+    T: float        # total time horizon
+    B: int          # batch size
+    K: int          # number of knots
+    dim: int        # dimensionality of the spline
+
+    # initialize the spline class
+    def __init__(self, Y0: jnp.ndarray, T: float):
+
+        # set the spline parameters parameters
+        self.Y = Y0
+        self.T = np.round(T, decimals=6)
+
+        # sizes from initial knots
+        self.B, self.K, self.dim = Y0.shape
+
+    # abstract method to evaluate the spline
+    @abstractmethod
+    def evaluate(self, times: jnp.ndarray) -> jnp.ndarray:
+        """
+        Evaluate the spline at given times.
+
+        Args:
+            times: jnp.array, shape (M,), times to evaluate the spline at.
+
+        Returns:
+            Y_eval: jnp.array, shape (B, M, dim), spline values at the given times.
+        """
+        raise NotImplementedError("evaluate method must be implemented in spline subclass.")
+
+    # update spline knot points
+    def update_knots(self, Y_new: jnp.ndarray):
+        """
+        Update the spline knot points.
+
+        Args:
+            Y_new: jnp.array, shape (B, K, dim), new knot points.
+        """
+
+        # check that the new shape matches
+        if Y_new.shape != self.Y.shape:
+            raise ValueError(f"Y_new shape {Y_new.shape} != existing shape {self.Y.shape}")
+        
+        # set the new knots
+        self.Y = Y_new
+
+#############################################################
+# Zero-Order Hold Spline
+#############################################################
+
+class ZOH_Spline(Base_Spline):
     """
     Zero-order hold spline.
 
@@ -23,62 +84,85 @@ class ZOH_Spline:
     Knot points are aligned to the left.
     """
 
-    # spline parameters
-    T: float                    # total time
-    Y: jnp.ndarray              # knot points
-    t_knots: jnp.ndarray        # knot times
-
     def __init__(self, Y0: jnp.ndarray, 
                        T: float):
         
-        # get sizes from the initial knots
-        self.B, self.K, self.dim = Y0.shape # (B, num_knots, dim)
-        
-        # set parameters
-        self.Y = Y0
+        # initialize parent class
+        super().__init__(Y0, T)
 
         # create the uniform knot times
-        self.T = T
-        self.t_knots = jnp.linspace(0.0, T, self.K) # (K,)
-
-        # initialize jit functions
-        # self.evaluate = jax.jit(self._evaluate)
-        self.evaluate = self._evaluate
+        self.dt = self.T / self.K
+        self.t_knots = jnp.arange(self.K) * self.dt   # [0, dt, 2dt, ... (K-1)dt]
 
         print("ZOH Spline initialized.")
 
     # evaluate the spline at given times
-    def _evaluate(self, times):
-        """
-        Given, a set of times, evaluate the ZOH spline at those times.
-        
-        Args: 
-            times: jnp.array, shape (M,) - times to evaluate the spline at.
-        Returns:
-            Y_eval: jnp.array, shape (B, M, nu) - spline values at the given times.
-        """
+    def evaluate(self, times):
 
         # ensure times is an array and clip to [0, T]
         times = jnp.clip(times, 0.0, self.T)
 
-        # Map times -> knot index k in [0, K-1]
-        k = jnp.searchsorted(self.t_knots, times, side="right") - 1
-        k = jnp.clip(k, 0, self.K - 1)  # (M,)
+        # interval index k = floor(t / dt)
+        k = jnp.floor(times / self.dt).astype(jnp.int32)
+        k = jnp.clip(k, 0, self.K - 1)
 
         # Gather along axis=1 (knot axis): (B, K, dim) -> (B, M, dim)
         return jnp.take(self.Y, k, axis=1)
     
-    # update spline knot points
-    def update_knots(self, Y_new: jnp.ndarray):
-        """
-        Update the spline knot points.
 
-        Args:
-            Y_new: jnp.array, shape (B, K, dim) - new knot points.
-        """
-        self.Y = Y_new
+#############################################################
+# Bezier Curve
+#############################################################
 
-    
+# class Bezier_Spline:
+#     """
+#     Bezier curve spline.
+
+#     The knot points are uniformly over [0, T].
+#     Knot points are aligned to the left.
+#     """
+
+#     def __init__(self, Y0: jnp.ndarray, 
+#                        T: float):
+        
+#         # get sizes from the initial knots
+#         self.B, self.K, self.dim = Y0.shape # (B, num_knots, dim)
+        
+#         # set parameters
+#         self.Y = Y0
+
+#         # create the uniform knot times
+#         self.T = T
+
+#         # initialize jit functions
+#         # self.evaluate = jax.jit(self._evaluate)
+#         self.evaluate = self._evaluate
+
+#         print("ZOH Spline initialized.")
+
+#     # evaluate the spline at given times
+#     def _evaluate(self, times):
+#         """
+#         Given, a set of times, evaluate the ZOH spline at those times.
+        
+#         Args: 
+#             times: jnp.array, shape (M,) - times to evaluate the spline at.
+#         Returns:
+#             Y_eval: jnp.array, shape (B, M, nu) - spline values at the given times.
+#         """
+
+#         # ensure times is an array and clip to [0, T]
+#         times = jnp.clip(times, 0.0, self.T)
+
+#         # Map times -> knot index k in [0, K-1]
+#         k = jnp.searchsorted(self.t_knots, times, side="right") - 1
+#         k = jnp.clip(k, 0, self.K - 1)  # (M,)
+
+#         # Gather along axis=1 (knot axis): (B, K, dim) -> (B, M, dim)
+#         return jnp.take(self.Y, k, axis=1)
+
+
+
 #############################################################
 # EXAMPLE USAGE
 #############################################################
@@ -90,9 +174,9 @@ if __name__ == "__main__":
 
     # create a ZOH spline
     B = 64
-    K = 10
+    K = 50
     nu = 2
-    T = 5.0
+    T = 10.0
     Y0 = jax.random.uniform(jax.random.PRNGKey(0), (B, K, nu), minval=-1.0, maxval=1.0)
     spline = ZOH_Spline(Y0, T)
 
