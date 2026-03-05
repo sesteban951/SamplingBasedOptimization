88af1e7d7760cfe5d48f9c7150e9b7517e0e0fe1
sesteban951 <sesteban@caltech.edu>
2026-03-05 15:26:02 -0800

    small edits in SRB traj opt

--- srb/srb_jump_2D.py
+++ srb/srb_jump_2D.py
@@ -164,7 +164,7 @@ p0_L = ca.DM([x0[0]])  # left  foot x
 p0_R = ca.DM([x0[0]])  # right foot x
 
 # Goal state: jumped forward, same height, upright, stopped
-px_goal = 1.0
+px_goal = 0.0
 x_goal = np.array([
     px_goal, pz_nom,  # px, pz,
     0.0,              # theta
